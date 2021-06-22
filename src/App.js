d03cf346bfdf4127bf31f86883ef9b55afac9bfa
Ulises Cervantes <ulises.cerv@hotmail.com>
2021-06-22 13:19:18 -0500

    Basic Route structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router,Switch  } from 'react-router-dom';
+import Home from './Pages/Home/Home'
+import NotFound from './Components/NotFound/NotFound'
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      Hello React!      
+      <Router>
+          <Switch>
+              <Route path="/" exact component={Home} />   
+              <Route path="*" component={NotFound} />  
+          </Switch>
+        </Router>    
     </div>
   );
 }
---
+++ src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './NotFound.scss'
+export default function NotFound() {
+    return (
+        <div className='NotFound'>
+            404
+        </div>
+    )
+}
