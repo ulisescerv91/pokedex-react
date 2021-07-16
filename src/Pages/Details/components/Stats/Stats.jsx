4e5b2733d7f36094d918b90320d29d2f3cc699eb
ulisescerv91 <ulises.cerv@hotmail.com>
2021-07-15 21:51:52 -0500

    progress bar - starting

--- src/Pages/Details/components/Stats/Stats.jsx
+++ src/Pages/Details/components/Stats/Stats.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Stats.scss'
 export default function Stats() {
+    const [style, setStyle] = useState({});
+    setTimeout(() => {
+		const newStyle = {
+			opacity: 1,
+			width: `${75}%`
+		}
+		
+		setStyle(newStyle);
+	}, 200);
     return (
         <div className='Stats'>
-            stats
+            <div className="Stats__container">
+                {/* HP */}
+                <div className="Stats__container__left">HP</div>
+                <div className="Stats__container__right">
+                    <div className="progress">
+                        <div className="progress-done" style={style}>
+                            {75}%
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
